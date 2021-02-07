71696b0ac07224a1e4214a0590c293c985bea740
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-02-06 17:11:55 -0700

    array challenge

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Sort an object
 const maxSpeed = {
   car: 300,
   bike: 60,
@@ -7,8 +8,67 @@ const maxSpeed = {
   rocket: 8 * 60 * 60,
 };
 
+// const sortedObj = Object.entries(maxSpeed)
+//   .sort(([, a], [, b]) => b - a)
+//   .reduce((a, [b, c]) => ({ ...a, [b]: c }), {});
+
+// console.log(sortedObj);
+
 const sortedObj = Object.entries(maxSpeed)
   .sort(([, a], [, b]) => b - a)
   .reduce((a, [b, c]) => ({ ...a, [b]: c }), {});
 
-console.log(sortedObj);
+// console.log(sortedObj);
+
+/*
+2D array
+Write an algorithm which searches through a 2D array, and whenever it finds a zero should 
+set the entire row and column to zero.
+```
+let input =
+[[1,0,1,1,0],
+[0,1,1,1,0],
+[1,1,1,1,1],
+[1,0,1,1,1],
+[1,1,1,1,1]];
+```
+Output:
+[ [ 0, 0, 0, 0, 0 ],
+ [ 0, 0, 0, 0, 0 ],
+ [ 0, 0, 1, 1, 0 ],
+ [ 0, 0, 0, 0, 0 ],
+ [ 0, 0, 1, 1, 0 ] ]
+
+ */
+let input = [
+  [1, 0, 1, 1, 0],
+  [0, 1, 1, 1, 0],
+  [1, 1, 1, 1, 1],
+  [1, 0, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+];
+
+function makeZero(arr) {
+  // colArr [true, ]
+  const colArr = [],
+    rowArr = [];
+  for (let a = 0; a < arr.length; a++) {
+    for (let b = 0; b < arr[a].length; b++) {
+      if (arr[a][b] === 0) {
+        rowArr[a] = true;
+        colArr[b] = true;
+      }
+    }
+  }
+  for (let a = 0; a < arr.length; a++) {
+    for (let b = 0; b < arr[a].length; b++) {
+      if (colArr[b] || rowArr[a]) {
+        arr[a][b] = 0;
+      }
+    }
+  }
+  return arr;
+  // console.log(colArr, rowArr);
+}
+
+console.log(makeZero(input));
