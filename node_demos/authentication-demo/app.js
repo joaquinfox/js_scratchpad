f2d7fdea2f737998f118077d99912b39dea209c8
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-02-07 22:01:30 -0700

    auth demo

--- node_demos/authentication-demo/app.js
+++ node_demos/authentication-demo/app.js
@@ -1,23 +1,72 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const User = require('./models/user');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+mongoose.connect('mongodb://localhost/dev_demo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set('view engine', 'ejs');
+app.use(
+  session({
+    secret: 'fjdka',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+app.get('/', (req, res) => {
+  res.render('landing');
+});
 app.get('/secret', (req, res) => {
   res.render('secret');
 });
 app.get('/register', (req, res) => {
   res.render('register');
 });
-app.post('/register', (req, res) => {});
+app.post('/register', (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.render('register');
+      }
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secret');
+      });
+    }
+  );
+});
 app.get('/login', (req, res) => {
   res.render('login');
 });
-app.post('/login', (req, res) => {});
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/register',
+  }),
+  (req, res) => {
+    res.render('login');
+  }
+);
 app.get('/logout', (req, res) => {
   //   res.render('landing');
+  req.logout();
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
