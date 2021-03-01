a91df201ff83e4f67128c9e4fbbb6de0f95df296
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-03-01 15:10:18 -0700

    updates

--- node_demos/fs_core/index.js
+++ node_demos/fs_core/index.js
@@ -120,3 +120,18 @@
 //     return a;
 //   }, {});
 // console.log(output);
+
+// const maxSpeed = {
+//   car: 300,
+//   bike: 60,
+//   motorbike: 200,
+//   airplane: 1000,
+//   helicopter: 400,
+//   rocket: 8 * 60 * 60,
+// };
+
+// let output = Object.entries(maxSpeed)
+//   .sort(([, a], [, b]) => b - a)
+//   .reduce((a, [b, c]) => ({ ...a, [b]: c }), {});
+
+// console.log(output);
